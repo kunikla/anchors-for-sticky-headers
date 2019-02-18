23b16ccbe45d76964bcf9bf9a0378b551c9f84f4
Christina Hamilton <kunikla.vt@gmail.com>
2019-02-18 13:40:05 -0500

    initial commit

---
+++ anchors-for-sticky-headers.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chamilton
+ * Date: 2/18/19
+ * Time: 1:29 PM
+ */
+
+// Get the shortcode
+
+// Find the header
+
+// Get its height
+
+// Build the code
+
+// Replace the shortcode with the code
